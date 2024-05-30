91ad2aa7162a6d66ccbb9d72da7f5ec09424a336
Denis Malinin <melowetty@mail.ru>
2024-05-30 12:01:50 +0500

    initial commit

---
+++ src/main/kotlin/ru/melowetty/ujineventalerter/UjinEventAlerterApplication.kt
@@ -0,0 +1,11 @@
+package ru.melowetty.ujineventalerter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UjinEventAlerterApplication
+
+fun main(args: Array<String>) {
+    runApplication<UjinEventAlerterApplication>(*args)
+}
---
+++ src/test/kotlin/ru/melowetty/ujineventalerter/UjinEventAlerterApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.melowetty.ujineventalerter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UjinEventAlerterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
